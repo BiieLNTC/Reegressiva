a382ff220de9c7f17c7539b3e08106de34b942fe
Gabriel Squissato <biel.squissato@hotmail.com>
2024-10-21 19:10:54 -0300

    evento atualizadado

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 import { Countdown } from "./contador.js"; 
 
-const tempoFesta = new Countdown("19 october 2024 19:00:00 GMT-0300")
+const tempoFesta = new Countdown("16 november 2024 19:00:00 GMT-0300")
 const tempos = document.querySelectorAll("[data-time]")
 console.log(mostrarTempo)
 
